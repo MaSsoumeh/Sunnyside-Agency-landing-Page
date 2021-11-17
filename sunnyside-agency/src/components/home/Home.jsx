0fa8a3ba02b0f001e8fc0588274dd511f68cf846
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-17 23:50:50 +0330

    create arrow and home page slang

--- sunnyside-agency/src/components/home/Home.jsx
+++ sunnyside-agency/src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import './home.scss';
 import headerImg from '../../images/mobile/image-header.jpg';
-
+import arrowImg from '../../images/icon-arrow-down.svg';
 const Home = () => {
   return (
     <>
@@ -10,6 +10,12 @@ const Home = () => {
           alt='a slice of an orange'
           className='headerImage'
         />
+        <img
+          src={arrowImg}
+          alt='an arrow icon point to an orange'
+          className='arrow'
+        />
+        <p className='creativeText'>WE ARE CREATIVES</p>
       </section>
     </>
   );
