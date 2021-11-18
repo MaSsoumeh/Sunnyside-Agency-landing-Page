afbedd148c85149fefe0082d55111623455c5f29
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-18 20:10:00 +0330

    create grid component

--- sunnyside-agency/src/components/grid/Grid.jsx
+++ sunnyside-agency/src/components/grid/Grid.jsx
@@ -1,5 +1,18 @@
+import bottle from '../../images/desktop/image-gallery-milkbottles.jpg';
+import orange from '../../images/desktop/image-gallery-orange.jpg';
+import cone from '../../images/desktop/image-gallery-cone.jpg';
+import SugarCube from '../../images/desktop/image-gallery-sugarcubes.jpg';
 const Grid = () => {
-  return <></>;
+  return (
+    <>
+      <div className='imageWrapper'>
+        <img src={bottle} alt='milk bottles' />
+        <img src={orange} alt='an orange in a bowl' />
+        <img src={cone} alt='cone' />
+        <img src={SugarCube} alt='sugar cubes' />
+      </div>
+    </>
+  );
 };
 
 export default Grid;
